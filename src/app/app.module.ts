9ff2e6031f7e3c3b7b7e55507c8f4aef7528c04d
llamar <thatlivelyllama@gmail.com>
2022-04-11 16:14:12 +0100

    responsive css changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { PlaceholderTextComponent } from './components/placeholder-text/placehol
 import { PlaceholderContactInfoComponent } from './components/placeholder-contact-info/placeholder-contact-info.component';
 import { PlaceholderLocationInfoComponent } from './components/placeholder-location-info/placeholder-location-info.component';
 import { LeaderboardComponent } from './components/leaderboard/leaderboard.component';
-import { LeaderboardPlayerComponent } from './components/leaderboard-player/leaderboard-player.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatTableModule} from '@angular/material/table';
 import {MatSortModule} from '@angular/material/sort';
@@ -20,7 +19,6 @@ import { HttpClientModule } from '@angular/common/http';
     PlaceholderContactInfoComponent,
     PlaceholderLocationInfoComponent,
     LeaderboardComponent,
-    LeaderboardPlayerComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/leaderboard/leaderboard.component.ts
+++ src/app/components/leaderboard/leaderboard.component.ts
@@ -21,8 +21,6 @@ export class LeaderboardComponent implements AfterViewInit {
   
   constructor(private _liveAnnouncer: LiveAnnouncer) {
     function mergeById(arrayPlayers: any, arrayResults: any) {
-      // array1.map((player:any) => console.log(player))
-      // console.log(array2)
       let results = arrayPlayers.map((player: any) => ({
         ...arrayResults.find((result: any) => (result.PlayerId === player.PlayerId) && result),
         ...player
@@ -40,18 +38,6 @@ export class LeaderboardComponent implements AfterViewInit {
       return array
     }
 
-    // async function fetchPackaging(): Promise<object> {
-    //   return await fetch("https://mocki.io/v1/57c703ab-c5af-4530-b126-d1b784353bea").then(res => res.json())
-    //     // .then(res => {
-    //     //   return res.json()
-    //     // })
-    //     // .then(data => {
-    //     //       players = mergeById(players, data.Results);
-    //     //       players = addPositions(players)
-    //     // })      
-      
-    // }
-
     fetch("https://mocki.io/v1/57c703ab-c5af-4530-b126-d1b784353bea")
       .then(data => data.json())
       .then(data => {
@@ -68,10 +54,6 @@ export class LeaderboardComponent implements AfterViewInit {
   }
 
   announceSortChange(sortState: Sort) {
-    // This example uses English messages. If your application supports
-    // multiple language, you would internationalize these strings.
-    // Furthermore, you can customize the message to add additional
-    // details about the values being sorted.
     if (sortState.direction) {
       this._liveAnnouncer.announce(`Sorted ${sortState.direction}ending`);
     } else {
